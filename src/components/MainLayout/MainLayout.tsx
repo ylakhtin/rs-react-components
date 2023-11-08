60cc9bebc0af018b4f580c68dabdc95ba60d58ba
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-11-08 16:03:13 +0600

    refactor: move styles for main layout and cover back with image

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,10 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import classes from './MainLayout.module.css';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import Search from '../Search/Search';
-import { Outlet } from 'react-router-dom';
 
 const MainLayout = function () {
   return (
-    <div className="main-space">
+    <div className={classes.mainSpace}>
       <ErrorBoundary
         fallback={
           'Error, please, reload the page! This is to test Error boundary! This is a fallback message!'
